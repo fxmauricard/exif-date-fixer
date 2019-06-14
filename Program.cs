29f1ecf6689b1d6a93f9fb40982b71f052eb2ec2
François-Xavier MAURICARD <fxmauricard@gmail.com>
2019-06-14 15:19:54 +0200

    Initial .NET Core App template generated by Visual Studio 2019.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ExifDateFixer
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
